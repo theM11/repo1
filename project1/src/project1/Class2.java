fbf904f3d9a3a6cbcd6115c6dc989e7d21f62118
jonmi <jonmi@DESKTOP-M8KST1F>
2016-03-29 19:49:14 +0200

    dfdsf

---
+++ project1/src/project1/Class2.java
@@ -0,0 +1,22 @@
+package project1;
+
+public class Class2 {
+	private int cislo;
+	private int cislo2;
+	
+	public int getCislo()
+	{
+		return cislo;
+	}
+	
+	public void setCislo(int cislo)
+	{
+		this.cislo = cislo;
+	}
+
+	public void setDouble1(int double1) {
+		this.cislo2 = double1;
+	}
+	
+	
+}
